c1bb0b4df8e0cc976bc7407bcfc67bbe542bdd48
Your Name <1137870473@qq.com>
2018-09-27 17:31:35 +0800

    new class

---
+++ app/src/main/java/com/example/administrator/githubdemo4/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.githubdemo4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator  on 2018/9/27
+ */
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
