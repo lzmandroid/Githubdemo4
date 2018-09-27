c45836976ab66474644caf2615f78815fcd82b47
Your Name <1137870473@qq.com>
2018-09-27 17:34:24 +0800

    change

--- app/src/main/java/com/example/administrator/githubdemo4/MainActivity.java
+++ app/src/main/java/com/example/administrator/githubdemo4/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.administrator.githubdemo4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String num;
+    private TextView tv_test;
+
+    private Button btnTets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
