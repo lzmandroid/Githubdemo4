ecf47846e91c7cca3b7da8d7b0aa5356fddc1be0
Your Name <1137870473@qq.com>
2018-10-04 10:10:21 +0800

    change

--- app/src/main/java/com/example/administrator/githubdemo4/TestActivity.java
+++ app/src/main/java/com/example/administrator/githubdemo4/TestActivity.java
@@ -2,16 +2,21 @@ package com.example.administrator.githubdemo4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Administrator  on 2018/9/27
  */
 public class TestActivity extends Activity {
+    private String aa;
+    private TextView tv_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aa="ssss";
 
     }
 }
