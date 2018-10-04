4e80752e416795257ee6bcdacdbfedddd487b156
Your Name <1137870473@qq.com>
2018-10-04 10:18:33 +0800

    change2

--- app/src/main/java/com/example/administrator/githubdemo4/TestActivity.java
+++ app/src/main/java/com/example/administrator/githubdemo4/TestActivity.java
@@ -11,19 +11,13 @@ import android.widget.TextView;
  * Created by Administrator  on 2018/9/27
  */
 public class TestActivity extends Activity {
-    private String aa;
-    private TextView tv_test;
 
-    private Button btn_test;
-    private ImageView img_test;
-    private EditText et_tests;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        aa="ssss";
 
     }
 }
